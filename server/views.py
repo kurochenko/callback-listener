cc7833487e136078ec56e2c86876f2f2eeb0c16f
Andrej Kuročenko <kurochenko@gmail.com>
2016-06-08 14:13:37 +0200

    Ignore env.

--- server/views.py
+++ server/views.py
@@ -1,34 +1,47 @@
 import json
 
-from flask import Response
+from flask import Response, request
 from server import app
 
 
-@app.route('/success')
+@app.route('/success', methods = ['POST'])
 def success():
+	prettyPrint(request.data)
 	return jsonOK()
 
 
-@app.route('/failure')
+@app.route('/failure', methods = ['POST'])
 def failure():
+	prettyPrint(request.data)
 	return jsonOK()
 
 
-@app.route('/notify')
+@app.route('/notify', methods = ['POST'])
 def notify():
+	prettyPrint(request.data)
 	return jsonOK()
 
 
-@app.route('/interactive')
+@app.route('/interactive', methods = ['POST'])
 def interactive():
+	prettyPrint(request.data)
 	return jsonOK()
 
 
-@app.route('/destroy')
+@app.route('/destroy', methods = ['POST'])
 def destroy():
+	prettyPrint(request.data)
 	return jsonOK()
+
+
+def prettyPrint(requestBody):
+	print('Calling endpoint: {ep}. Body ⤵'.format(ep = request.endpoint))
+
+	jString = json.loads(requestBody.decode('utf-8'))
+	print(json.dumps(jString, sort_keys = True, indent = 2))
+	print()
 
 
 def jsonOK():
-	return Response(json.dumps({'status': 'OK'})),  mimetype='application/json')
+	return Response(json.dumps({'status': 'OK'}),  mimetype='application/json')
 
